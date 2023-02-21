85a09f6b8edf792995f0e07e64d1ac5f2dc4c3ce
Nicolas20221020055 <125925104+Nicolas20221020055@users.noreply.github.com>
2023-02-21 18:54:30 -0500

    Update Pruebagithub.java

--- src/pruebagithub/Pruebagithub.java
+++ src/pruebagithub/Pruebagithub.java
@@ -6,6 +6,8 @@ public class Pruebagithub {
     public static void main(String[] args) {
         System.out.println("Prueba numero 1");
         System.out.println("Hola Mundo");
+        
+        System.out.println("Nice");
     }
     
 }
